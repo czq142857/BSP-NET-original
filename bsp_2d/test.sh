d00c5406dc3cc4bbc277e9dcc7620132a0fe3b3e
Zhiqin Chen <2741678221@qq.com>
2020-03-20 10:08:55 -0700

    ipython -> python

--- bsp_2d/test.sh
+++ bsp_2d/test.sh
@@ -1,1 +1,1 @@
-ipython main.py -- --phase 1 --start 0 --dataset complex_elements --data_dir ./data/ --checkpoint_dir checkpoint --sample_dir samples/complex_elements --sample_vox_size 64
+python main.py --phase 1 --start 0 --dataset complex_elements --data_dir ./data/ --checkpoint_dir checkpoint --sample_dir samples/complex_elements --sample_vox_size 64
--- bsp_2d/train_without_L_overlap.sh
+++ bsp_2d/train_without_L_overlap.sh
@@ -1,3 +1,3 @@
-ipython main.py -- --train --phase 0 --iteration 1000000 --dataset complex_elements --data_dir ./data/ --checkpoint_dir checkpoint --sample_dir samples/complex_elements0 --sample_vox_size 64
+python main.py --train --phase 0 --iteration 1000000 --dataset complex_elements --data_dir ./data/ --checkpoint_dir checkpoint --sample_dir samples/complex_elements0 --sample_vox_size 64
 
-ipython main.py -- --train --phase 1 --iteration 1000000 --dataset complex_elements --data_dir ./data/ --checkpoint_dir checkpoint --sample_dir samples/complex_elements1 --sample_vox_size 64
+python main.py --train --phase 1 --iteration 1000000 --dataset complex_elements --data_dir ./data/ --checkpoint_dir checkpoint --sample_dir samples/complex_elements1 --sample_vox_size 64
